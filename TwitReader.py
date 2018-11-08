2fa31d8f7dfaf8d243fa28438347dcdb25148e0d
OmegaKi113r <omegaki113r@gmail.com>
2018-11-08 19:56:40 +0530

    Issues were corrected of postInTime function

--- TwitReader.py
+++ TwitReader.py
@@ -4,27 +4,24 @@ import threading
 from auth import(consumer_key,consumer_secret,access_token,access_token_secret)
 
 numberOfPosts=0
-
-def openFile():
-    global numberOfPosts
-    tweetsFile = open('C:/Users/OmegaKiller/Desktop/twitter/posts.txt', 'r')
-    tweets = tweetsFile.readlines()
-    numberOfPosts = len(tweets)
-    tweetsFile.close()
-
-
-
 i=0
-
 twitter=Twython(consumer_key,consumer_secret,access_token,access_token_secret)
 
+
+tweetsFile = open('C:/Users/OmegaKiller/Desktop/twitter/posts.txt', 'r')
+tweets = tweetsFile.readlines()
+
+numberOfPosts = len(tweets)
+tweetsFile.close()
 
 
 
 def postInTime():
     global i
 
-    threading.Timer(18*60,postInTime).start()
+    print i
+
+    threading.Timer(2*60,postInTime).start()
     splitted=tweets[i].split(',')
 
     print splitted[0].rstrip()
@@ -34,14 +31,18 @@ def postInTime():
     response=twitter.upload_media(media=photo)
     twitter.update_status(status= splitted[0].rstrip(),media_ids=[response['media_id']])
 
-    ++i
+    i=i+1
+
+    print i
 
     if i>=len(tweets):
         i=0
 
+        print i
+    print i
 
 
-openFile()
+
 postInTime()
 
 
